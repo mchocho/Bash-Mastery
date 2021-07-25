a89295d92fdc1d063f99d61e36b0ff7dd6e65eff
sal'e chocho <mchocho@student.wethinkcode.co.za>
2021-07-25 14:18:02 +0100

    Adding scripts to Path

---
+++ 01-Just-Like-Movie-Scripts/Adding-scripts-to-your-path/scripts/script.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+
+# Author: Malesela Chocho
+# Date Created: 24/07/2021
+# Lasr Modifed: 24/07/2021
+
+# Description
+# Prints "This is our first script" to the terminal
+
+# Usage
+# script
+
+echo "This is our first script"
+
+# Exit statuses can have any value from 0 - 255
+# Exit code 0 indicates that the script has executed succesfully
+# Any non zero exit code indicates that something went wrong
+#          https://tldp.org/LDP/abs/html/exitcodes.html
+
+exit 0
