fc82dde866a86ccfda4d385c6aa7d18590d8dc58
sal'e chocho <mchocho@student.wethinkcode.co.za>
2021-07-29 19:28:59 +0100

    Command substitution

---
+++ 02-Variables-and-expansions/Command-substituton/script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Command subsitution is very similar to parameter expansion
+# except instead of typing variable name we write out full
+# commands and get the output of the command instead.
+
+# The syntax for a command substitution is as follows:
+#        $(command)
+
+# This will cause the shell to run the command and substitute
+# the dollar parentheses with the standard out of the command
+# we run
+
+time=$(date +%H:%m:%S)
+
+echo "Hey there $USER, the time right now is $time"
+
+exit 9
