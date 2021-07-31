0584e94dc48a636966da5d35c77e3a84f1f0478d
sal'e chocho <mchocho@student.wethinkcode.co.za>
2021-07-31 19:52:25 +0100

    Tilde expansion

---
+++ 02-Variables-and-expansions/Tilde-expansion/script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Tilde expansion allows us to easily refer to the current
+# user's home directory
+#      echo ~
+#      echo ~root
+
+#      echo $PWD	 displays current directory
+#      echo ~+           a shorthand for $PWD
+
+#      echo $OLDPWD      displays previous directory before cd
+#      echo ~-           a shorthand for $OLDPWD
+
+# To continuously change between current directory and previous
+# directory you can run
+#      cd ~-
+
+exit 0
