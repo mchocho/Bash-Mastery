96840411ece77c922e0eaa13c371caff58a2321c
sal'e chocho <mchocho@student.wethinkcode.co.za>
2021-08-18 12:38:50 +0100

    If statements project

---
+++ 05-Logic/06-If-Statements-Project/memory_logger.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ -d $HOME/performance ]; then
+  echo "$HOME/performance folder exists"
+else
+  mkdir $HOME/performance
+  echo "$HOME/performance folder has been created"
+fi
+
+free >> $HOME/performance/memory.log
+exit 0
