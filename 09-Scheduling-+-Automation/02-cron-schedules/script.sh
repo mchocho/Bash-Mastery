1ed71586762297a5d4203d82a8b437b0202c29d8
sal'e chocho <mchocho@student.wethinkcode.co.za>
2021-08-31 19:11:53 +0100

    Scheduling tasks with cron (incomplete)

---
+++ 09-Scheduling-+-Automation/02-cron-schedules/script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# The crontab is used to automate all types
+# of tasks on Linux systems.
+
+# You write rows to the cron table when entering
+# a crontab command. Each ‘*’ asterisk represents
+# a segment of time and a corresponding column in
+# each row.
+
+date >> cron_output.txt
