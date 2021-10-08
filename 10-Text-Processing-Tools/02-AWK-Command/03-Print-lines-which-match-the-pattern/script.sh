c7e8fadec777a569840bbe7b846373427628121a
sal'e chocho <mchocho@student.wethinkcode.co.za>
2021-10-08 23:49:33 +0200

    Print lines which match the provided pattern

--- 10-Text-Processing-Tools/02-AWK-Command/03-Print-lines-which-match-the-pattern/script.sh
+++ 10-Text-Processing-Tools/02-AWK-Command/03-Print-lines-which-match-the-pattern/script.sh
@@ -2,9 +2,29 @@
 
 printUsage() {
   echo "Usage $0 -n"
-  echo "    where -n is the values to match."
+  echo "    where -n is the values to match using space as a delimeter."
 
   exit 1
 }
 
+if [ $# -eq 0 ]; then
+  printUsage
+fi
 
+str='/'
+index=1
+
+while [ $index -le $# ]; do
+  if [ $index -gt 1 ]; then
+    str="${str}|"
+  fi
+  str="${str}${!index}"
+
+  index=$((index + 1))
+done
+
+str="${str}/"
+
+echo "Command: awk '${str}' ../sample.txt"
+echo
+echo `awk ${str} ../sample.txt`
